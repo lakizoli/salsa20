9845c6c96c402fccf417ec258f63bd58d48de672
Laki <zlaki@graphisoft.com>
2017-12-13 08:56:44 +0100

    some organization

--- salsa20/main.cpp
+++ salsa20/main.cpp
@@ -1,6 +1,29 @@
 //salsa20 speedup project
 
+#include <experimental/filesystem>
+#include <cassert>
+#include <iostream>
+
+namespace fs = std::experimental::filesystem;
+using namespace std;
+
+#define SUCCESS 0
+#define ERROR 1
+
+static int PrintUsage (char* exePath) {
+	cout << "Usage:" << endl << endl << fs::absolute (exePath).filename ().string () << " <source file path> <target file path>" << endl << endl;
+	return ERROR;
+}
+
 int main (int argc, char* argv[]) {
-    return 0;
+	//Read parameters
+	if (argc < 3) {
+		assert (argc >= 1);
+		return PrintUsage (argv[0]);
+	}
+
+	//Do reference cipher
+
+    return SUCCESS;
 }
 
